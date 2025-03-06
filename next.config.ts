70dcf836e500cf5f20b312f7827efc72800c888a
hereis.topdev <hereis.topdev@gmail.com>
2025-03-06 12:08:45 -0500

    Fix redirect URL

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/ComingSoon',
+        permanent: true, // permanent (301) or temporary (302)
+      },
+    ];
+  },
 };
 
 export default nextConfig;
